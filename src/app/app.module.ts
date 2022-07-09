28679c7bc67f3bc1a05af4a195036abe9e108e9c
MatiasO <matiaz.orellana@gmail.com>
2022-07-09 15:17:41 -0300

    220709 creando lista de secciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
